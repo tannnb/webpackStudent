5e53500803d9893fb60e55dd3d674a07bcaa8202
tannnb <tanbingwork@163.com>
2019-04-23 22:38:48 +0800

    增加热更新配置

--- lesson10/README.md
+++ lesson10/README.md
@@ -6,3 +6,9 @@
 
 使用webpackdevserver 不会生成dist，插件会吧dist放到内存中读取出来
 
+
+devServer配置了hot hotOnly,热更新插件才会生效
+
+// 在devServer开启了hot和hotOnly 配置热更新插件，才会生效
+// 可以在写css和js的时候 方便时时调试样式
+new webpack.HotModuleReplacementPlugin(),
--- lesson10/src/index.js
+++ lesson10/src/index.js
@@ -1,1 +1,14 @@
-console.log('hello world###')
+import './index.css'
+
+
+var oBtn = document.createElement('button')
+oBtn.innerText = '点击'
+document.body.appendChild(oBtn)
+
+oBtn.onclick = function () {
+  var oItem = document.createElement('div')
+  oItem.innerText = 'item'
+  document.body.appendChild(oItem)
+
+}
+
