4a65d53c4c4196327bc21cc6580ba647e571751d
tannnb <tanbingwork@163.com>
2019-05-05 12:47:43 +0800

    webpack性能优化1

---
+++ lesson25/README.md
@@ -0,0 +1,7 @@
+# webpack 性能优化
+
+1.减少loader引入
+
+2.plugin尽可能精简并确保可靠
+
+
---
+++ lesson25/build/webpack.common.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: {
+		main: './src/index.js',
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			// 排除node_modules 的loader检测，可以提升webpack速度
+			exclude: /node_modules/,
+			use: [{
+				loader: 'babel-loader'
+			}]
+		}, {
+			test: /\.(jpg|png|gif)$/,
+			use: {
+				loader: 'url-loader',
+				options: {
+					name: '[name]_[hash].[ext]',
+					outputPath: 'images/',
+					limit: 10240
+				}
+			}
+		}, {
+			test: /\.(eot|ttf|svg)$/,
+			use: {
+				loader: 'file-loader'
+			}
+		}]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'src/index.html'
+		}),
+		new CleanWebpackPlugin(['dist'], {
+			root: path.resolve(__dirname, '../')
+		}),
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			_join: ['lodash', 'join']
+		}),
+	],
+	optimization: {
+		runtimeChunk: {
+			name: 'runtime'
+		},
+		usedExports: true,
+		splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+      	vendors: {
+      		test: /[\\/]node_modules[\\/]/,
+      		priority: -10,
+      		name: 'vendors',
+      	}
+      }
+    }
+	},
+	performance: false,
+	output: {
+		path: path.resolve(__dirname, '../dist')
+	}
+}
